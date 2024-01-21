c7201a429a235b67691db36e5f25c649ac5f9a73
gnujoow <gnujoow.kim@gmail.com>
2024-01-21 22:07:59 +0900

    mock claim and refetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,38 @@
 import { useReadClaimKey } from "@/hooks/readContract";
 import { useAccount, useConnect, useDisconnect } from "wagmi";
 import { injected } from "wagmi/connectors";
+import { useSignMessage } from "wagmi";
+import { claimKey } from "@/hooks/writeContract";
 
 function App() {
   const account = useAccount();
-  const { connectors, connect, status, error } = useConnect();
+  const { connect, status, error } = useConnect();
   const { disconnect } = useDisconnect();
+  const { signMessageAsync } = useSignMessage();
 
-  const { data: claimedKeyData } = useReadClaimKey();
+  const { data: claimedKeyData, refetch } = useReadClaimKey();
   console.info({ claimedKeyData });
+
+  const handleClickClaim = async () => {
+    try {
+      if (!account.address) return;
+      // sign message
+      const signData = await signMessageAsync({ message: "yeah" });
+      console.info({ signData });
+      const truncatedHexString = signData.substr(2, 64);
+      const sign32byte = `0x${truncatedHexString}` as `0x${string}`;
+
+      console.info({ sign32byte });
+      // claim key
+      const data = await claimKey(sign32byte);
+      console.info({ data });
+
+      // refetch
+      await refetch();
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   return (
     <>
@@ -23,6 +47,8 @@ function App() {
           addresses: {JSON.stringify(account.addresses)}
           <br />
           chainId: {account.chainId}
+          <div>status :{status}</div>
+          <div>{error?.message}</div>
         </div>
 
         {account.status === "connected" && (
@@ -39,8 +65,21 @@ function App() {
         >
           connect
         </button>
-        <div>{status}</div>
-        <div>{error?.message}</div>
+
+        <hr />
+        <button onClick={handleClickClaim}>Claim</button>
+        <hr />
+      </div>
+
+      <div>
+        <h2>Claimed Key</h2>
+        <div>
+          {claimedKeyData?.map((item) => (
+            <div key={item.claimedAt}>
+              <img src={item.tokenURI} alt="" />
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
---
+++ src/hooks/writeContract.ts
@@ -0,0 +1,12 @@
+import { abi } from '@/abi'
+import { config } from '@/wagmi'
+import { writeContract } from '@wagmi/core'
+
+export const claimKey = (args: `0x${string}`) => {
+  return writeContract(config, {
+    abi,
+    address: '0x01B620e6BA56D0C66ED64fC6667C67a6E538D50f',
+    functionName: 'claimKey',
+    args: [args],
+  })
+}
