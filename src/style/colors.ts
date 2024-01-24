cf4a03688d4bf98164f5e6601fe519f6401866d6
gnujoow <gnujoow.kim@gmail.com>
2024-01-25 02:42:35 +0900

    define colors

---
+++ src/style/colors.ts
@@ -0,0 +1,14 @@
+const black = '#000';
+
+
+const colors = {
+  black,
+  gray10: '#EEE',
+  gray30: '#CACACA',
+  primaryLight10: '#60ABCB',
+  primaryLight30: '#EEF9FD',
+  primary: '#556981',
+  steelBlueDark: '#838AA6',
+};
+
+export default colors;
