1e4bd3cfdb293c0a9e6115c5e72ccacf1dac53e9
gnujoow <gnujoow.kim@gmail.com>
2024-01-25 00:54:25 +0900

    add axios to fetch tokenURI

---
+++ src/vendor/axios/index.ts
@@ -0,0 +1,11 @@
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
+
+const axiosInstance = axios.create();
+
+export interface HttpClient extends AxiosInstance {
+  get<T = unknown>(url: string, config?: AxiosRequestConfig): Promise<T>;
+  post<T = unknown>(url: string, data?: any, config?: AxiosRequestConfig): Promise<T>;
+}
+
+export const instance: HttpClient = axiosInstance;
+instance.interceptors.response.use(res => res.data);
