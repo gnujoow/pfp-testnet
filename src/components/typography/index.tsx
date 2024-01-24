72b347832f3848e96ebc752331b68dbe0726a026
gnujoow <gnujoow.kim@gmail.com>
2024-01-25 02:46:26 +0900

    define typography

---
+++ src/components/typography/index.tsx
@@ -0,0 +1,35 @@
+import styled from "@emotion/styled";
+
+export const Heading5 = styled.h5`
+  font-family: Quantico;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 34px; /* 141.667% */
+  letter-spacing: 0.48px;
+`;
+export const Caption1 = styled.p`
+  font-family: Quantico;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 20px; /* 142.857% */
+  letter-spacing: 0.28px;
+`;
+export const Caption2 = styled.p`
+  /* caption2 */
+  font-family: Quantico;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 18px; /* 150% */
+  letter-spacing: 0.48px;
+`;
+export const ButtonLabel = styled.span`
+  font-family: Quantico;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 26px; /* 144.444% */
+  letter-spacing: 0.36px;
+`;
